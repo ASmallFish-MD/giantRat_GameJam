eb718970584aa24fc54db716b2212fe0751d5cd3
Miron Zadora <mironandrzej1@gmail.com>
2021-05-28 10:28:55 +0100

    Make WallSpawner spawn walls

--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -6,24 +6,52 @@ public class WallSpawner : MonoBehaviour {
     public float scrollSpeed;
     public Vector2 scrollDirection = Vector2.down;
 
-    public float offScreenYPos = -10;
+    public float offScreenYPos = -10f;
+
+    public List<Sprite> wallSprites;
+    public List<Sprite> oceanWallSprites;
+    public Transform wallPrefab;
+
+    public float wallXPos = 8.5f;
+
+    public float generateWallUntillY = 10f;
+
+    public float maxYofWallGenerated;
 
 
     void Start(){
-
+        maxYofWallGenerated = -10f;
     }
 
     // Update is called once per frame
     void Update() {
         //TODO: getting all walls each fram may be inefficient...
         int nWalls = this.gameObject.transform.childCount;
+        Vector2 scrollAmount = scrollDirection * Time.deltaTime * scrollSpeed;
+        maxYofWallGenerated += scrollAmount.y;
         for(int i = 0; i < nWalls; i++){
             Transform wall = this.gameObject.transform.GetChild(i);
-            wall.Translate(scrollDirection * Time.deltaTime * scrollSpeed);
+            wall.Translate(scrollAmount);
             if(wall.transform.position.y < offScreenYPos){
-                //change sprite and teleport up
-                //TODO
+                Object.Destroy(wall.gameObject);
             }
         }
+        if(maxYofWallGenerated<generateWallUntillY){
+            spawnWall();
+        }
+    }
+
+    void spawnWall(){
+        Transform spawnedWall = Instantiate(wallPrefab, new Vector3(-wallXPos, maxYofWallGenerated), Quaternion.identity);
+        spawnedWall.SetParent(transform);
+        spawnedWall.GetComponent<SpriteRenderer>().sprite = wallSprites[Random.Range(0, wallSprites.Count)];
+        
+
+        spawnedWall = Instantiate(wallPrefab, new Vector3(wallXPos, maxYofWallGenerated), Quaternion.identity);
+        spawnedWall.SetParent(transform);
+        spawnedWall.GetComponent<SpriteRenderer>().sprite = wallSprites[Random.Range(0, wallSprites.Count)];
+        spawnedWall.GetComponent<SpriteRenderer>().flipX = true;
+
+        maxYofWallGenerated+=spawnedWall.GetComponent<SpriteRenderer>().size.y;
     }
 }
