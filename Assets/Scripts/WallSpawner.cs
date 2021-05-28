29c7e97a833b43464bcefa421c884e857cae88eb
Miron Zadora <mironandrzej1@gmail.com>
2021-05-28 03:16:00 +0100

    add scrolling to WallSpawner

--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallSpawner : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class WallSpawner : MonoBehaviour {
+    public float scrollSpeed;
+    public Vector2 scrollDirection = Vector2.down;
+
+    public float offScreenYPos = -10;
+
+
+    void Start(){
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        //TODO: getting all walls each fram may be inefficient...
+        int nWalls = this.gameObject.transform.childCount;
+        for(int i = 0; i < nWalls; i++){
+            Transform wall = this.gameObject.transform.GetChild(i);
+            wall.Translate(scrollDirection * Time.deltaTime * scrollSpeed);
+            if(wall.transform.position.y < offScreenYPos){
+                //change sprite and teleport up
+                //TODO
+            }
+        }
     }
 }
