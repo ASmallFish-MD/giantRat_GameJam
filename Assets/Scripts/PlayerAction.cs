da601f124ccfa00cbc4a9e165907af70beb2cc00
Miron Zadora <mironandrzej1@gmail.com>
2021-05-28 06:40:04 +0100

    add function to get closest object

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerAction : MonoBehaviour{
     public Transform GrabSlot;
 
+    public float grabRange = 1.5f;
+
     public enum actionType{
         Nothing,
         Dropping,
@@ -20,12 +22,6 @@ public class PlayerAction : MonoBehaviour{
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public actionType performPlayerAction(){
         bool holding = GrabSlot.childCount > 0;
         Transform actionableObject = null;
@@ -34,7 +30,11 @@ public class PlayerAction : MonoBehaviour{
         if(holding){
             actionableObject = GrabSlot.GetChild(0);
         }else{
-            //TODO actionableObject = getClosestObject();
+            actionableObject = getClosestObject();
+        }
+
+        if(actionableObject == null){
+            return actionType.Nothing;
         }
 
         if(actionableObject.CompareTag("Rock")){
@@ -70,5 +70,47 @@ public class PlayerAction : MonoBehaviour{
         }
 
         return returnAction;
+    }
+
+    private Transform getClosestObject(){
+        Transform actionableObjectsTransform = GameObject.FindGameObjectWithTag("ActionableObjectsTransform").transform;
+
+        //https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.sortedlist-2?view=net-5.0
+        SortedList<float, Transform> actionableObjects = new SortedList<float, Transform>();
+        //SortedList for storing the objects already in the level
+        //keys = object x pos, values = object transform
+
+        //make a list of objects on the platform to grab
+        int nGrabbableObjects = actionableObjectsTransform.childCount;
+        for(int i = 0; i < nGrabbableObjects; i++){
+            Transform actionableObject = actionableObjectsTransform.GetChild(i);
+            if(actionableObject.CompareTag("Rock") && actionableObject.GetComponent<RockScript>().rockState == RockScript.state.OnPlatform
+                || actionableObject.CompareTag("Duck") && actionableObject.GetComponent<DuckScript>().duckState == DuckScript.state.OnPlatform){
+                actionableObjects.Add(actionableObject.position.x, actionableObject);
+                Debug.Log(actionableObject);
+            }
+        }
+
+        bool facingRight = Mathf.Sign(this.gameObject.transform.localScale.x) > 0; //TODO: get facing right from another source
+        float plrXPos = this.gameObject.transform.position.x;
+
+        Transform closestObject = null;
+
+        //linear search through grabbable object list to find closest one
+        if(facingRight){
+            for (int i = 0; i < actionableObjects.Count; i++){
+                if(plrXPos <= actionableObjects.Keys[i] && actionableObjects.Keys[i] <= plrXPos + grabRange){
+                    closestObject = actionableObjects.Values[i];
+                }
+            }
+        }else{ //if facing left, search in the reverse order
+            for (int i = 0; i < actionableObjects.Count; i++){
+                if(plrXPos - grabRange <= actionableObjects.Keys[actionableObjects.Count-i-1] && actionableObjects.Keys[actionableObjects.Count-i-1] <= plrXPos){
+                    closestObject = actionableObjects.Values[actionableObjects.Count-i-1];
+                }
+            }
+        }
+        
+        return closestObject;
     }
 }
