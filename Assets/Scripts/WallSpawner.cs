f31e3f07ba0dc05688fa130f02079e1292f0bf57
Miron Zadora <mironandrzej1@gmail.com>
2021-06-02 03:30:48 +0100

    Make ocean tiles spawn more frequently with progress

--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -19,8 +19,11 @@ public class WallSpawner : MonoBehaviour {
     public float maxYofWallGenerated;
 
 
+    Transform beach;
+
     void Start(){
         maxYofWallGenerated = -10f;
+        beach = GameObject.FindGameObjectWithTag("TheBeach").transform;
     }
 
     // Update is called once per frame
@@ -36,22 +39,33 @@ public class WallSpawner : MonoBehaviour {
                 Object.Destroy(wall.gameObject);
             }
         }
-        if(maxYofWallGenerated<generateWallUntillY){
-            spawnWall();
+        if(maxYofWallGenerated < generateWallUntillY){
+            spawnWallPair();
         }
     }
 
-    void spawnWall(){
-        Transform spawnedWall = Instantiate(wallPrefab, new Vector3(-wallXPos, maxYofWallGenerated), Quaternion.identity);
-        spawnedWall.SetParent(transform);
-        spawnedWall.GetComponent<SpriteRenderer>().sprite = wallSprites[Random.Range(0, wallSprites.Count)];
-        
+    void spawnWallPair(){
+        Transform spawnedWall;
 
-        spawnedWall = Instantiate(wallPrefab, new Vector3(wallXPos, maxYofWallGenerated), Quaternion.identity);
-        spawnedWall.SetParent(transform);
-        spawnedWall.GetComponent<SpriteRenderer>().sprite = wallSprites[Random.Range(0, wallSprites.Count)];
-        spawnedWall.GetComponent<SpriteRenderer>().flipX = true;
+        for(int i = -1; i < 2; i += 2){
+            spawnedWall = Instantiate(wallPrefab, new Vector3(i * wallXPos, maxYofWallGenerated), Quaternion.identity);
+            spawnedWall.SetParent(transform);
 
-        maxYofWallGenerated+=spawnedWall.GetComponent<SpriteRenderer>().size.y;
+            float chanceOfOceanTile = Mathf.Lerp(0f, 0.97f, Mathf.Pow(Mathf.InverseLerp(20f, 140f, 150f-beach.position.y), 2f));
+            Sprite wallSprite;
+            if(Random.value < chanceOfOceanTile){
+                wallSprite = oceanWallSprites[Random.Range(0, oceanWallSprites.Count)];
+            }else{
+                wallSprite = wallSprites[Random.Range(0, wallSprites.Count)];
+            }
+
+            spawnedWall.GetComponent<SpriteRenderer>().sprite = wallSprite;
+
+            if(i > 0){
+                spawnedWall.GetComponent<SpriteRenderer>().flipX = true;
+
+                maxYofWallGenerated += spawnedWall.GetComponent<SpriteRenderer>().size.y;
+            }
+        }
     }
 }
